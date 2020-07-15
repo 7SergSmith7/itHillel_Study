230c019c81c6d6957d47d0d5ba69dea86d529a1f
7SergSmith7 <serg.kuznetsov89@gmail.com>
2020-07-16 01:08:58 +0300

    new

---
+++ homework_final/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp"),
+  postcss = require("gulp-postcss"),
+  autoprefixer = require("autoprefixer"),
+  rigger = require("gulp-rigger");
+
+gulp.task("myHtml", function () {
+  return gulp.src("src/*.html").pipe(rigger()).pipe(gulp.dest("build"));
+});
+
+gulp.task("myCss", function () {
+  return gulp
+    .src("src/css/styles.css")
+    .pipe(postcss([autoprefixer]))
+    .pipe(gulp.dest("build/styles"));
+});
+
+gulp.task("myImages", function () {
+  return gulp.src("src/images/*/*").pipe(gulp.dest("build/images"));
+});
+
+gulp.task("myFonts", function () {
+  return gulp.src("src/fonts/*").pipe(gulp.dest("build/fonts"));
+});
+
+gulp.task(
+  "myFinalBuild",
+  gulp.series(["myHtml", "myCss", "myImages", "myFonts"])
+);
