a75764667a82ba76e1b7a9c6fb2c2facfc761bb6
7SergSmith7 <serg.kuznetsov89@gmail.com>
2020-08-11 09:58:04 +0300

    Update script.js

--- Homework_JS.08/script.js
+++ Homework_JS.08/script.js
@@ -1,7 +1,7 @@
 function getSimpleNumbers(number) {
   let arrayNumbers = getArrayNumbers(number);
   let arrayNaturalNumbers = [];
-  for (let i = 0; i < arrayNumbers.length; i++) {
+  for (let i = 1; i < arrayNumbers.length; i++) {
     if (checkPrime(arrayNumbers[i])) {
       arrayNaturalNumbers.push(arrayNumbers[i]);
     }
@@ -30,8 +30,8 @@ function checkNumber() {
   let userInput;
   let flag = true;
   do {
-    userInput = +prompt("Enter rigth number(>0).");
-    if (Number.isInteger(userInput) && userInput > 0) flag = false;
+    userInput = +prompt("Please enter a positive integer number and > 1.");
+    if (Number.isInteger(userInput) && userInput > 1) flag = false;
   } while (flag);
 
   return userInput;
